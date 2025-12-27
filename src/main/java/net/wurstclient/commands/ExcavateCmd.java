b718cc7a7a2ba0423c50305cbe716bafb7426101
VOREGA <72041595+VOREGA@users.noreply.github.com>
2025-12-27 12:13:13 +0300

    yenilik

--- src/main/java/net/wurstclient/commands/ExcavateCmd.java
+++ src/main/java/net/wurstclient/commands/ExcavateCmd.java
@@ -1,55 +1,42 @@
-/*
- * Copyright (c) 2014-2025 Wurst-Imperium and contributors.
- *
- * This source code is subject to the terms of the GNU General Public
- * License, version 3. If a copy of the GPL was not distributed with this
- * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
- */
 package net.wurstclient.commands;
 
-import net.minecraft.core.BlockPos;
-import net.wurstclient.command.CmdException;
-import net.wurstclient.command.CmdSyntaxError;
+import net.wurstclient.WurstClient;
 import net.wurstclient.command.Command;
-import net.wurstclient.util.MathUtils;
+import net.wurstclient.hacks.ExcavatorHack;
 
 public final class ExcavateCmd extends Command
 {
-	public ExcavateCmd()
-	{
-		super("excavate",
-			"Automatically destroys all blocks in the selected area.",
-			".excavate <x1> <y1> <z1> <x2> <y2> <z2>");
-	}
-	
-	@Override
-	public void call(String[] args) throws CmdException
-	{
-		if(args.length != 6)
-			throw new CmdSyntaxError();
-		
-		BlockPos pos1 = argsToXyzPos(args[0], args[1], args[2]);
-		BlockPos pos2 = argsToXyzPos(args[3], args[4], args[5]);
-		WURST.getHax().excavatorHack.enableWithArea(pos1, pos2);
-	}
-	
-	private BlockPos argsToXyzPos(String... xyz) throws CmdSyntaxError
-	{
-		BlockPos playerPos = BlockPos.containing(MC.player.position());
-		int[] player = {playerPos.getX(), playerPos.getY(), playerPos.getZ()};
-		int[] pos = new int[3];
-		
-		for(int i = 0; i < 3; i++)
-			if(MathUtils.isInteger(xyz[i]))
-				pos[i] = Integer.parseInt(xyz[i]);
-			else if(xyz[i].equals("~"))
-				pos[i] = player[i];
-			else if(xyz[i].startsWith("~")
-				&& MathUtils.isInteger(xyz[i].substring(1)))
-				pos[i] = player[i] + Integer.parseInt(xyz[i].substring(1));
-			else
-				throw new CmdSyntaxError("Invalid coordinates.");
-			
-		return new BlockPos(pos[0], pos[1], pos[2]);
-	}
+    public ExcavateCmd()
+    {
+        super("excavate", "Özel koordinatlı kazıyı başlatır.",
+            ".excavate <x1> <y1> <z1> <x2> <y2> <z2>");
+    }
+
+    @Override
+    public void call(String[] args) throws Exception
+    {
+        if(args.length != 6)
+        {
+            addErrorMessage("Hatalı kullanım! Örnek:");
+            addErrorMessage(".excavate 6491 62 -3089 6516 54 -3069");
+            return;
+        }
+
+        // Sayıları oku
+        int x1 = Integer.parseInt(args[0]);
+        int y1 = Integer.parseInt(args[1]);
+        int z1 = Integer.parseInt(args[2]);
+        int x2 = Integer.parseInt(args[3]);
+        int y2 = Integer.parseInt(args[4]);
+        int z2 = Integer.parseInt(args[5]);
+
+        // Excavator hilesini bul
+        ExcavatorHack hack = WurstClient.INSTANCE.getHax().excavatorHack;
+        
+        // Hileyi senin koordinatlarınla başlat
+        hack.setEnabled(true);
+        hack.startCustomExcavate(x1, y1, z1, x2, y2, z2);
+        
+        addMessage("Kazı hedefi ayarlandı ve başlatıldı.");
+    }
 }
